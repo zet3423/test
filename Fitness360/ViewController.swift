90006f3ced933886417b3d20dbc920c952bfee7a
Sam KC <skc4@masonlive.gmu.edu>
2020-04-02 01:10:01 -0400

    Initial Commit

---
+++ Fitness360/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fitness360
+//
+//  Created by Sam KC on 4/2/20.
+//  Copyright © 2020 BitBoard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
